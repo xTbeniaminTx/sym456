8edf3d70b5a7823996cef671fec5461e2fbfcad4
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2020-07-25 19:33:43 +0200

    Service Container Advanced ROUTES

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use App\Services\GiftsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -22,6 +23,42 @@ class DefaultController extends AbstractController
             'users' => $users,
             'random_gift' => $gifts->gifts
         ]);
+    }
+
+    /**
+     * @Route("/blog/{page}", name="blog_list", requirements={"page"="\d+"})
+     */
+    public function index2()
+    {
+        return new Response("Optional parameters in url and requiremants for parameters!");
+    }
+
+    /**
+     * @Route(
+     *     "/articles/{_locale}/{year}/{slug}/{category}",
+     *     defaults={"category": "computers"},
+     *     requirements={
+     *          "_locale": "en|fr",
+     *          "category": "computers|rtv",
+     *          "year": "\d+"
+     *        }
+     * )
+     *
+     */
+    public function index3()
+    {
+        return new Response("Advance route!");
+    }
+
+    /**
+     * @Route({
+     *     "nl": "/over-ons",
+     *     "en": "/about-us"
+     *     }, name="about_us")
+     */
+    public function index4()
+    {
+        return new Response("Advance route 4 --- Translated routes!");
     }
 
 }
