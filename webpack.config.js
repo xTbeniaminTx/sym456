687d453a5a7a9fa9803218142322b2769877f5ff
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2020-07-27 12:47:40 +0200

    webpack encore

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('js/custom', './build/js/custom.js')
+    .addStyleEntry('css/custom', './build/css/custom.css')
+
+
+;
+
+module.exports = Encore.getWebpackConfig();
