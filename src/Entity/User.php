a98024e289a9b0dc429729e2f5d6a9f2b1f70596
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2020-07-29 01:21:49 +0200

    many to many self referencing relationship

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -49,9 +49,21 @@ class User
      */
     private $address;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=User::class, inversedBy="following")
+     */
+    private $followed;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=User::class, mappedBy="followed")
+     */
+    private $following;
+
     public function __construct()
     {
         $this->videos = new ArrayCollection();
+        $this->followed = new ArrayCollection();
+        $this->following = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -110,6 +122,60 @@ class User
     public function setAddress(?Address $address): self
     {
         $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getFollowed(): Collection
+    {
+        return $this->followed;
+    }
+
+    public function addFollowed(self $followed): self
+    {
+        if (!$this->followed->contains($followed)) {
+            $this->followed[] = $followed;
+        }
+
+        return $this;
+    }
+
+    public function removeFollowed(self $followed): self
+    {
+        if ($this->followed->contains($followed)) {
+            $this->followed->removeElement($followed);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getFollowing(): Collection
+    {
+        return $this->following;
+    }
+
+    public function addFollowing(self $following): self
+    {
+        if (!$this->following->contains($following)) {
+            $this->following[] = $following;
+            $following->addFollowed($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFollowing(self $following): self
+    {
+        if ($this->following->contains($following)) {
+            $this->following->removeElement($following);
+            $following->removeFollowed($this);
+        }
 
         return $this;
     }
