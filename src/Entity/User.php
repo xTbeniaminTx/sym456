2922344ab4d8f5e5470fa275d01db78bcf4a19c0
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2020-07-28 23:49:54 +0200

    Address entity with relation to User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -44,6 +44,11 @@ class User
      */
     private $videos;
 
+    /**
+     * @ORM\OneToOne(targetEntity=Address::class, cascade={"persist", "remove"})
+     */
+    private $address;
+
     public function __construct()
     {
         $this->videos = new ArrayCollection();
@@ -93,6 +98,18 @@ class User
                 $video->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getAddress(): ?Address
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?Address $address): self
+    {
+        $this->address = $address;
 
         return $this;
     }
